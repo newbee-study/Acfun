e6e4eae8b1d5389481a6e1189089349be329c181
hww <hww@163.com>
2016-05-09 21:00:45 +0800

    创建自定义TabBarController

---
+++ Acfun/Acfun/Classes/Main/Controller/ACTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  ACTabBarController.h
+//  Acfun
+//
+//  Created by 黄文伟 on 16/5/9.
+//  Copyright © 2016年 黄文伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACTabBarController : UITabBarController
+
+@end
